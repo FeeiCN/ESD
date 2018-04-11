16768af764d2709187bc28d89fcf7e797ffc28d7
Feei <feei@feei.cn>
2018-04-11 14:11:52 +0800

    fix #7 基于RSC（响应相似度比对）支持枚举泛解析域名

--- ESD.py
+++ ESD.py
@@ -16,13 +16,21 @@ import re
 import sys
 import time
 import string
+import random
+import traceback
 import itertools
 import datetime
 import colorlog
 import asyncio
 import aiodns
+import aiohttp
 import logging
+import requests
+import async_timeout
+from aiohttp.resolver import AsyncResolver
+from itertools import islice
 from logging import handlers
+from difflib import SequenceMatcher
 
 log_path = 'logs'
 if os.path.isdir(log_path) is not True:
@@ -52,7 +60,7 @@ file_handler.setFormatter(formatter)
 logger = colorlog.getLogger('ESD')
 logger.addHandler(handler)
 logger.addHandler(file_handler)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 
 class EnumSubDomain(object):
@@ -62,6 +70,7 @@ class EnumSubDomain(object):
         logger.info('Start domain: {d}'.format(d=domain))
         self.data = {}
         self.domain = domain
+        self.stable_dns_servers = ['8.8.8.8', '114.114.114.114', '1.1.1.1']
         dns_servers = []
         dns_server_config = '{pd}/servers.esd'.format(pd=self.project_directory)
         if not os.path.isfile(dns_server_config):
@@ -71,13 +80,54 @@ class EnumSubDomain(object):
             for s in f:
                 dns_servers.append(s.strip())
         if len(dns_servers) == 0:
-            logger.info('ESD/servers.esd not configured, The default will be used!')
-            dns_servers = ['223.5.5.5', '223.6.6.6', '114.114.114.114']
+            logger.info('ESD/servers.esd not configured, The default dns server will be used!')
+            dns_servers = self.stable_dns_servers
+        random.shuffle(dns_servers)
+        self.dns_servers = dns_servers
         self.loop = asyncio.get_event_loop()
-        self.resolver = aiodns.DNSResolver(loop=self.loop, nameservers=dns_servers)
+        self.resolver = aiodns.DNSResolver(loop=self.loop, nameservers=self.dns_servers)
         self.general_dicts = []
+        # Mark whether the current domain name is a pan-resolved domain name
+        self.is_wildcard_domain = False
+        # Use a nonexistent domain name to determine whether
+        # there is a pan-resolve based on the DNS resolution result
+        self.wildcard_sub = 'feei-esd-{random}'.format(random=random.randint(0, 9999))
+        # There is no domain name DNS resolution IP
+        self.wildcard_ips = []
+        # No domain name response HTML
+        self.wildcard_html = None
+        self.wildcard_html_len = 0
+        # Subdomains that are consistent with IPs that do not have domain names
+        self.wildcard_subs = []
+        # Wildcard domains use RSC
+        self.wildcard_domains = {}
+        # Corotines count
+        self.coroutine_count = None
+        self.coroutine_count_dns = 100000
+        self.coroutine_count_request = 100
+        # RSC ratio
+        self.rsc_ratio = 0.8
+        self.remainder = 0
+        # Reuqest Header
+        self.request_headers = {
+            'Connection': 'keep-alive',
+            'Pragma': 'no-cache',
+            'Cache-Control': 'no-cache',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'DNT': '1',
+            'Referer': 'http://www.baidu.com/robot',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8'
+        }
 
     def generate_general_dicts(self, line):
+        """
+        Generate general subdomains dicts
+        :param line:
+        :return:
+        """
         letter_count = line.count('{letter}')
         number_count = line.count('{number}')
         letters = itertools.product(string.ascii_lowercase, repeat=letter_count)
@@ -98,6 +148,10 @@ class EnumSubDomain(object):
             return self.general_dicts
 
     def load_sub_domain_dict(self):
+        """
+        Load subdomains from files and dicts
+        :return:
+        """
         dicts = []
         with open('{pd}/subs.esd'.format(pd=self.project_directory), encoding='utf-8') as f:
             for line in f:
@@ -115,11 +169,31 @@ class EnumSubDomain(object):
         return dicts
 
     async def query(self, sub):
+        """
+        Query domain
+        :param sub:
+        :return:
+        """
         full_domain = '{sub}.{domain}'.format(sub=sub, domain=self.domain)
         try:
-            ret = await self.resolver.query(full_domain, 'A')
-            ret = [r.host for r in ret]
-            self.process(sub, ret)
+            if sub == self.wildcard_sub:
+                # Use a stable DNS server to determine the ubiquitous resolution
+                resolver = aiodns.DNSResolver(loop=self.loop, nameservers=self.stable_dns_servers)
+                ret = await resolver.query(full_domain, 'A')
+                ret = [r.host for r in ret]
+            else:
+                ret = await self.resolver.query(full_domain, 'A')
+                ret = [r.host for r in ret]
+                domain_ips = [s for s in ret]
+                # It is a wildcard domain name and
+                # the subdomain IP that is burst is consistent with the IP
+                # that does not exist in the domain name resolution,
+                # the response similarity is discarded for further processing.
+                if self.is_wildcard_domain and sorted(self.wildcard_ips) == sorted(domain_ips):
+                    logger.debug('{r} maybe wildcard domain, continue RSC {sub}'.format(r=self.remainder, sub=sub, ips=domain_ips))
+                else:
+                    self.data[full_domain] = sorted(domain_ips)
+                    logger.info('{r} {sub} {ips}'.format(r=self.remainder, sub=sub, ips=domain_ips))
         except aiodns.error.DNSError as e:
             err_code, err_msg = e.args[0], e.args[1]
             # 1:  DNS server returned answer with no data
@@ -129,18 +203,30 @@ class EnumSubDomain(object):
             if err_code not in [1, 4, 11, 12]:
                 logger.info('{domain} {exception}'.format(domain=full_domain, exception=e))
             ret = None
-        logger.debug('query {sub}'.format(sub=sub))
+        self.remainder += -1
         return sub, ret
 
     def process(self, sub, ret):
+        """
+        Process result
+        :param sub:
+        :param ret:
+        :return:
+        """
         full_domain = '{sub}.{domain}'.format(sub=sub, domain=self.domain)
         domain_ips = [s for s in ret]
-        self.data[full_domain] = sorted(domain_ips)
-        logger.info('{domains} {ips}'.format(domains=full_domain, ips=domain_ips))
+        # It is a wildcard domain name and
+        # the subdomain IP that is burst is consistent with the IP
+        # that does not exist in the domain name resolution,
+        # the response similarity is discarded for further processing.
+        if self.is_wildcard_domain and sorted(self.wildcard_ips) == sorted(domain_ips):
+            logger.debug('{r} {sub} maybe wildcard domain, continue RSC'.format(r=self.remainder, sub=sub, ips=domain_ips))
+        else:
+            self.data[full_domain] = sorted(domain_ips)
+            logger.info('{r} {sub} {ips}'.format(r=self.remainder, sub=sub, ips=domain_ips))
 
     @staticmethod
     def limited_concurrency_coroutines(coros, limit):
-        from itertools import islice
         futures = [
             asyncio.ensure_future(c)
             for c in islice(coros, 0, limit)
@@ -164,36 +250,138 @@ class EnumSubDomain(object):
             yield first_to_finish()
 
     async def start(self, tasks):
-        for res in self.limited_concurrency_coroutines(tasks, 10000):
+        """
+        Limit the number of coroutines for reduce memory footprint
+        :param tasks:
+        :return:
+        """
+        for res in self.limited_concurrency_coroutines(tasks, self.coroutine_count):
             await res
 
-    def run(self):
-        start_time = time.time()
-        # extensive domain
-        job = self.query('enumsubdomain-feei')
-        sub, ret = self.loop.run_until_complete(job)
-        if ret is not None and len([s for s in ret]) > 0:
-            logger.critical('存在泛解析，无法通过枚举子域名爆破!')
+    @staticmethod
+    async def fetch(session, url):
+        """
+        Fetch url response with session
+        :param session:
+        :param url:
+        :return:
+        """
+        try:
+            async with async_timeout.timeout(10):
+                async with session.get(url) as response:
+                    return await response.text()
+        except Exception as e:
+            return None
+
+    async def similarity(self, sub):
+        """
+        Enumerate subdomains by responding to similarities
+        :param sub:
+        :return:
+        """
+        sub_domain = '{sub}.{domain}'.format(sub=sub, domain=self.domain)
+        full_domain = 'http://{sub_domain}'.format(sub_domain=sub_domain)
+        try:
+            resolver = AsyncResolver(nameservers=self.dns_servers)
+            conn = aiohttp.TCPConnector(resolver=resolver)
+            async with aiohttp.ClientSession(connector=conn, headers=self.request_headers) as session:
+                html = await self.fetch(session, full_domain)
+                if html is None:
+                    return
+                if len(html) == self.wildcard_html_len:
+                    ratio = 1
+                else:
+                    # SPEED 4 2 1
+                    # real_quick_ratio() > quick_ratio() > ratio()
+                    ratio = SequenceMatcher(None, html, self.wildcard_html).real_quick_ratio()
+                    ratio = round(ratio, 3)
+                self.remainder += -1
+                if ratio > self.rsc_ratio:
+                    logger.debug('{r} RSC ratio: {ratio} (passed) {sub}'.format(r=self.remainder, sub=sub, ratio=ratio))
+                else:
+                    logger.info('{r} RSC ratio: {ratio} (added) {sub}'.format(r=self.remainder, sub=sub, ratio=ratio))
+                    self.wildcard_domains[sub_domain] = html
+                    self.data[sub_domain] = self.wildcard_ips
+        except Exception as e:
+            logger.debug(traceback.format_exc())
             return
+
+    def distinct(self):
+        for domain, html in self.wildcard_domains.items():
+            for domain2, html2 in self.wildcard_domains.items():
+                ratio = SequenceMatcher(None, html, html2).real_quick_ratio()
+                if ratio > self.rsc_ratio:
+                    # remove this domain
+                    if domain2 in self.data:
+                        del self.data[domain2]
+                    m = 'Remove'
+                else:
+                    m = 'Stay'
+                logger.info('{d} : {d2} {ratio} {m}'.format(d=domain, d2=domain2, ratio=ratio, m=m))
+
+    def run(self):
+        """
+        Run
+        :return:
+        """
+        start_time = time.time()
         subs = self.load_sub_domain_dict()
+        self.remainder = len(subs)
         logger.info('Sub domain dict count: {c}'.format(c=len(subs)))
         logger.info('Generate coroutines...')
+        # wildcard domain
+        job = self.query(self.wildcard_sub)
+        sub, ret = self.loop.run_until_complete(job)
+        logger.info('{sub} {ret}'.format(sub=sub, ret=ret))
+        if ret is not None and len([s for s in ret]) > 0:
+            logger.info('This is a wildcard domain, will enumeration subdomains use by DNS+RSC.')
+            self.is_wildcard_domain = True
+            self.wildcard_ips = ret
+            try:
+                self.wildcard_html = requests.get('http://{w_sub}.{domain}'.format(w_sub=self.wildcard_sub, domain=self.domain), headers=self.request_headers, timeout=10).text
+                self.wildcard_html_len = len(self.wildcard_html)
+                logger.debug('Wildcard domain response html length: {len}'.format(len=self.wildcard_html_len))
+            except requests.exceptions.ConnectTimeout:
+                logger.warning('Request response content failed, check network please!')
+        self.coroutine_count = self.coroutine_count_dns
         tasks = (self.query(sub) for sub in subs)
         self.loop.run_until_complete(self.start(tasks))
+        dns_time = time.time()
+        time_consume_dns = int(dns_time - start_time)
+        # Response similarity comparison
+        dns_subs = []
+        for domain, ips in self.data.items():
+            logger.info('{domain} {ips}'.format(domain=domain, ips=ips))
+            dns_subs.append(domain)
+        self.wildcard_subs = list(set(subs) - set(dns_subs))
+        logger.info('Enumerates {len} sub domains by DNS mode in {tcd}.'.format(len=len(self.data), tcd=str(datetime.timedelta(seconds=time_consume_dns))))
+        logger.info('Will continue to test the {len} domains name for generically parsed IPs by responding to similarity ratios, the speed will be affected.'.format(len=len(self.wildcard_subs)))
+        self.coroutine_count = self.coroutine_count_request
+        self.remainder = len(self.wildcard_subs)
+        tasks = (self.similarity(sub) for sub in self.wildcard_subs)
+        self.loop.run_until_complete(self.start(tasks))
+
+        # Distinct last domains use RSC
+        # Maybe misinformation
+        # self.distinct()
+
+        time_consume_request = int(time.time() - dns_time)
+        logger.info('Requests time consume {tcr}s'.format(tcr=str(datetime.timedelta(seconds=time_consume_request))))
         # write output
         output_path_with_time = '{pd}/data/{domain}_{time}.esd'.format(pd=self.project_directory, domain=self.domain, time=datetime.datetime.now().strftime("%Y-%m_%d_%H-%M"))
         output_path = '{pd}/data/{domain}.esd'.format(pd=self.project_directory, domain=self.domain)
         with open(output_path_with_time, 'w') as opt, open(output_path, 'w') as op:
             for domain, ips in self.data.items():
-                # 格式和其它扫描器一致，确保被同时调用时不增加解析成本
+                # The format is consistent with other scanners to ensure that they are
+                # invoked at the same time without increasing the cost of resolution
                 con = '%-30s%-s\n' % (domain, ','.join(ips))
                 op.write(con)
                 opt.write(con)
         logger.info('Output: {op}'.format(op=output_path))
         logger.info('Output with time: {op}'.format(op=output_path_with_time))
         logger.info('Total domain: {td}'.format(td=len(self.data)))
-        time_consume = time.time() - start_time
-        logger.info('Time consume: {tc}s'.format(tc=round(time_consume, 3)))
+        time_consume = int(time.time() - start_time)
+        logger.info('Time consume: {tc}s'.format(tc=str(datetime.timedelta(seconds=time_consume))))
 
 
 if __name__ == '__main__':
