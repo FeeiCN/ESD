65143b431eeac73cae5ecfbb07da7f5fa0315345
zizhu <zizhu@meili-inc.com>
2018-12-06 21:02:52 +0800

    添加从 CA 证书获取子域名信息功能

--- ESD/__init__.py
+++ ESD/__init__.py
@@ -29,6 +29,7 @@ import aiohttp
 import logging
 import requests
 import backoff
+import socket
 import async_timeout
 from aiohttp.resolver import AsyncResolver
 from itertools import islice
@@ -60,6 +61,53 @@ logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
 ssl.match_hostname = lambda cert, hostname: True
+
+class CAInfo(object):
+    def __init__(self, domain):
+        self.domain = domain
+
+    def dns_resolve(self):
+        padding_domain = 'www.' + self.domain
+        # loop = asyncio.get_event_loop()
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        resolver = aiodns.DNSResolver(loop=loop)
+        f = resolver.query(padding_domain, 'A')
+        result = loop.run_until_complete(f)
+        return result[0].host
+
+    def get_cert_info_by_ip(self, ip):
+        s = socket.socket()
+        s.settimeout(10)
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        cert_path = base_dir + '/cacert.pem'
+        connect = ssl.wrap_socket(s, cert_reqs=ssl.CERT_REQUIRED, ca_certs=cert_path)
+        connect.settimeout(10)
+        connect.connect((ip, 443))
+        cert_data = connect.getpeercert().get('subjectAltName')
+        return cert_data
+
+    def get_ca_domain_info(self):
+        domain_list = list()
+        try:
+            ip = self.dns_resolve()
+            cert_data = self.get_cert_info_by_ip(ip)
+        except Exception as e:
+            return domain_list
+
+        for domain_info in cert_data:
+            hostname = domain_info[1]
+            if not hostname.startswith('*') and hostname.endswith(self.domain):
+                domain_list.append(hostname)
+
+        return domain_list
+
+    def get_subdomains(self):
+        subs = list()
+        subdomain_list = self.get_ca_domain_info()
+        for sub in subdomain_list:
+            subs.append(sub[:len(sub) - len(self.domain) - 1])
+        return subs
 
 
 class EnumSubDomain(object):
@@ -526,6 +574,14 @@ class EnumSubDomain(object):
         logger.info('Collect DNSPod JSONP API\'s subdomains...')
         domains = self.dnspod()
         logger.info('DNSPod JSONP API Count: {c}'.format(c=len(domains)))
+
+        # CA subdomain info
+        logger.info('Collect subdomains in CA...')
+        ca_subdomains = CAInfo(self.domain).get_subdomains()
+        tasks = (self.query(sub) for sub in ca_subdomains)
+        self.loop.run_until_complete(self.start(tasks))
+        logger.info('CA subdomain count: {c}'.format(c=len(ca_subdomains)))
+
         # write output
         tmp_dir = '/tmp/esd'
         if not os.path.isdir(tmp_dir):
