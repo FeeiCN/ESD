d21cbc916d6b7ff3bcd277dd01b1c0009107ed56
zizhu <zizhu@meili-inc.com>
2019-01-24 12:05:01 +0800

    更改版本，添加测试数据

--- ESD/__init__.py
+++ ESD/__init__.py
@@ -38,7 +38,7 @@ from aiohttp.resolver import AsyncResolver
 from itertools import islice
 from difflib import SequenceMatcher
 
-__version__ = '0.0.19'
+__version__ = '0.0.20'
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
