2b8de28850897896d032f08f8683d40ea2eaa260
ke0 <ke0ge7@gmail.com>
2020-03-26 15:22:45 +0800

    fix dns transfer

--- ESD/lib/dnsTransfer.py
+++ ESD/lib/dnsTransfer.py
@@ -12,7 +12,7 @@ class DNSTransfer(object):
             nameservers = [str(ns) for ns in nss]
             ns_addr = dns.resolver.query(nameservers[0], 'A')
             # dnspython 的 bug，需要设置 lifetime 参数
-            zones = dns.zone.from_xfr(dns.query.xfr(ns_addr, self.domain, relativize=False, timeout=2, lifetime=2), check_origin=False)
+            zones = dns.zone.from_xfr(dns.query.xfr(ns_addr.rrset[0].address, self.domain, relativize=False, timeout=2, lifetime=2), check_origin=False)
             names = zones.nodes.keys()
             for n in names:
                 subdomain = ''
