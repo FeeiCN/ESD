106dcd539f6f226e194c710dba0728e24d8cec5e
feei <feei@feei.cn>
2021-10-27 11:26:21 +0800

    add comments

--- ESD/__init__.py
+++ ESD/__init__.py
@@ -15,11 +15,11 @@
 import sys
 
 if __name__ == '__main__':
-    # Python版本判断，因采用了Asyncio等特性，仅在Python3及以上可运行。
+    # Check Python version, required python3 or higher because used asyncio feature
     if sys.version_info.major < 3:
         print('Python 3 or higher is required for ESD, upgrade your python please!')
         sys.exit(1)
-    # 提前引入Python3代码会导致报错
+    # Exception if before import Python3 code
     import EnumSubDomain
 
     EnumSubDomain.main()
